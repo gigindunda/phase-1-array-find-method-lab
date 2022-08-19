b89f64ef83e1d70b152d7444e519ac7c17790805
+ <+>
2022-08-19 07:43:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // code your solution here
 superbowlWin = (record) => {
     const result = record.find(record => record.result === "W");
-    return !!result ? result.year : undefined;
+    return !!result ? result.year : undefined
 }
